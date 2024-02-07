04107a846eba9736fd2aa664c8366b65527f9b6a
joefelx <joefelix.a2003@gmail.com>
2024-02-07 20:38:11 +0530

    Signup route created

--- app.ts
+++ app.ts
@@ -1,11 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import express from "express";
+import express, { application } from "express";
 import mongoose from "mongoose";
+import morgan from "morgan";
+
+import authRouter from "./routes/auth";
 
 const app = express();
 const PORT = 5000;
+
+app.use(express.json());
+app.use(morgan("tiny"));
 
 mongoose
   .connect(process.env.MONGO_URL!)
@@ -14,5 +20,6 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Hello from Express server!");
 });
+app.use("/auth", authRouter);
 
 app.listen(PORT, () => console.log(`Server started at port:${PORT}`));
--- routes/auth.ts
+++ routes/auth.ts
@@ -1,8 +1,62 @@
 import express from "express";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
 
 const router = express.Router();
 
-router.post("/sigup", (req, res) => {
-  const { name, email, password } = req.body;
+type StatusType =
+  | "SIGNED_UP"
+  | "LOGGED_IN"
+  | "LOGGED_OUT"
+  | "USER_EXISTS"
+  | "LOGIN_FAILED"
+  | "UNAUTHORISED"
+  | "INVALID_PASSWORD"
+  | "TOKEN_EXPIRED";
+
+interface ResponseType {
+  status: StatusType;
+  message: String | JSON | null;
+}
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const foundUser = await User.findOne({ email: email });
+
+    if (foundUser) {
+      const response: ResponseType = {
+        status: "USER_EXISTS",
+        message: "Go to Login",
+      };
+
+      return res.status(400).json(response);
+    }
+
+    // hashing the password
+    const saltRounds = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    // signing JWT token
+    const token = jwt.sign({ name, email }, process.env.JWT_SECRET!, {
+      expiresIn: "7d",
+    });
+
+    // storing user in the database
+    const user = new User({ name, email, password: hashedPassword, token });
+    const saverUser = await user.save();
+
+    const response: ResponseType = {
+      status: "SIGNED_UP",
+      message: saverUser.token,
+    };
+
+    res.status(201).json(response);
+  } catch (error) {
+    console.log(error);
+  }
 });
+
+export default router;
